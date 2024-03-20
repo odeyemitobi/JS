b4cac377d2e36371d03d3ffc8d99601adc0593c3
Odeyemi Tobiloba <odeyemioluwatobiloba11@gmail.com>
2024-03-20 15:10:03 +0100

    Leggo

---
+++ Typescript/trial.js
@@ -0,0 +1,13 @@
+var string = "Hello World Tobi";
+console.log(string);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
+var animals = ["cat", "dog"];
+animals.push("lion");
+console.log(animals);
+var mixed = ["Tobi", 12, true];
+mixed.push("dog", 12, false);
+console.log(mixed);
